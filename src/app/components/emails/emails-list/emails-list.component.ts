fa6298ddb712c264ccb68b3e0b7fb6de466123e5
Sserg <sergey.s.sergienko@gmail.com>
2017-05-16 22:43:37 +0300

    Pagination added

--- src/app/components/emails/emails-list/emails-list.component.ts
+++ src/app/components/emails/emails-list/emails-list.component.ts
@@ -6,8 +6,10 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styles: [require('./emails-list.component.css')]
 })
 export class EmailsList {
+  p: number = 5;
   @Input() emails;
   @Input() selectedEmail;
   @Output() onSelect = new EventEmitter();
   @Output() onDelete = new EventEmitter();
+  @Output() pageChange: EventEmitter<number>;
 }
--- src/app/components/emails/index.ts
+++ src/app/components/emails/index.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { Emails } from './emails.component';
 import { EmailsList } from './emails-list/emails-list.component';
 // import { EmailDetail } from './email-detail.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
-  imports: [ CommonModule ],
+  imports: [ CommonModule, NgxPaginationModule ],
   declarations: [ Emails, EmailsList ],
   exports: [ Emails ],
   providers: []
