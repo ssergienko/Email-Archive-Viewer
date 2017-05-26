372be484889b6801a8f194464307b7fa2c7fb02d
Sserg <sergey.s.sergienko@gmail.com>
2017-05-26 12:13:19 +0300

    Index.ts files added

--- src/app/components/emails-container/emails.module.ts
+++ src/app/components/emails-container/emails.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { EmailsContainer } from './emails.component';
-import { EmailsList } from './emails-list/emails-list.component';
-import { EmailComponent } from './emails-list/email/email.component';
+import { EmailsList } from './emails-list';
+import { EmailComponent } from './emails-list/email';
 
 import { Ng2OrderModule } from 'ng2-order-pipe';
 import { NgxPaginationModule } from 'ngx-pagination';
